b25f69dc09a0f4d8b019ed1e9e5c43b216c5c18c
Sairam Nagula <sairamnagulaharshit@gmail.com>
2025-12-17 14:57:37 -0500

    Fix: another playwright fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      "app/api/**": ["node_modules/@sparticuz/chromium/**"],
+      "api/**": ["node_modules/@sparticuz/chromium/**"],
+    },
+  } as any,
 };
 
 export default nextConfig;
